1ea57fad29f60af21bb9606e800f1b35c296a430
Claudio André Ferreria Caron <140202178+Claudio-Caron@users.noreply.github.com>
2023-09-13 13:58:37 -0400

    Create aula005.py

---
+++ exercícios curso de python/aula005.py
@@ -0,0 +1,13 @@
+import random
+#usar a função randrange da biblioteca random, com fito de gerar números aleatórios
+numi=6
+numf=5.21
+numc=1j
+x=numi
+print ("valor: ", x, "classe/tipo", type(x))
+y=[random.randrange (1, 57), random.randrange (1, 57), random.randrange (1, 57), random.randrange (1, 57), random.randrange (1, 57)]
+print ("num gerado 01 :", y[0])
+print ("num gerado 02 :", y[1])
+print ("num gerado 03 :", y[2])
+print ("num gerado 04 :", y[3])
+print ("num gerado 05 :", y[4])
