eed1f1af5f8f4086850eb20ffdb05abcf137498a
Claudio André Ferreria Caron <140202178+Claudio-Caron@users.noreply.github.com>
2023-09-13 00:06:30 -0400

    Create aula003.py

---
+++ exercícios curso de python/aula003.py
@@ -0,0 +1,8 @@
+"""abordado tipagem em Python, funções tipo set e range , list/array, dictionary, tuplas"""
+varbool= True
+varfloat=3.14159
+varint= 15
+varstr= "string"
+x=["carro", "moto", "caminhao", 14.7, 11, False]#list/ array
+print ("valor :", x[2])
+print ("tipo:", type (x))
