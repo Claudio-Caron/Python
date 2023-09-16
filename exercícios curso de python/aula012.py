a20a6dc7fcc3afd22398a895a334397e1bdf5354
Claudio André Ferreria Caron <140202178+Claudio-Caron@users.noreply.github.com>
2023-09-16 18:29:37 -0400

    Create aula012.py
    estrutura de repetição for em Python
    P

---
+++ exercícios curso de python/aula012.py
@@ -0,0 +1,6 @@
+#laço for, estrutura de loop , repetição
+lista= ["celta", "gol", "belina", "wit", "is", "since", "smaller"]
+print ("\n\nlista :\n")
+for c in range (0,7):
+    print (lista[c].upper())
+print ("fim_alg")
