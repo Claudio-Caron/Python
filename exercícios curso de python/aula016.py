a76f66ce355aea33f6c18eb3f8018d15e1d99572
Claudio André Ferreria Caron <140202178+Claudio-Caron@users.noreply.github.com>
2023-09-19 10:58:54 -0400

    Update aula016.py

--- exercícios curso de python/aula016.py
+++ exercícios curso de python/aula016.py
@@ -1,3 +1,11 @@
 #matrizes de arrays
 assembly=["heavy","wit","unbeatable"]
 print ("\n", assembly[1][2])
+meals=[["breakfast", "coffe"],
+       ["lunch", "peanut_sandwich"],
+       ["snack", "shake_protein"],
+       ["dinner", "montain_of_food"] 
+       ]
+for line,column in meals:
+    print ("refeição :", line, "-> food :", column)
+print ("\n Fim_alg")
