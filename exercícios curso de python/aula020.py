78570d1bfdeea697f83cf87113c7157849a3c981
Claudio André Ferreria Caron <140202178+Claudio-Caron@users.noreply.github.com>
2023-09-23 15:55:25 -0400

    Update aula020.py
    alterações aula 20

--- exercícios curso de python/aula020.py
+++ exercícios curso de python/aula020.py
@@ -1,38 +1,50 @@
-#função com retorno
 import os
-os.system ("cls")
-cash=1500.00
+os.system("cls")
+cash=1500
 op=1
-def simulator():    
+def simulator():  #mensagem de banco/SEM RETORNO  
     print ("\tSIMULADOR DE CAIXA ELETRÔNICO\n\n")
-def balance():
-    print ("saldo atual: {}".format(cash))
-    return -1
-def sucessfull():
+def balance():#saldo-COM RETORNO
+    print ("saldo atual:", cash)
+def sucessfull(y):#mensagem para operação realizada ou não
     os.system ("cls")
     simulator()
-    print ("\nOPERAÇÃO REALIZADA COM SUCESSO!!!")
-    x=balance()
-def withdrawal ():
-    x=balance()
+    if y==0:
+        print ("\nOPERAÇÃO CANCELADA\n")
+    else :
+        print ("\nOPERAÇÃO REALIZADA COM SUCESSO!!!")
+    balance()
+def withdrawal ():#saque
+    os.system ("cls")
+    simulator()
+    balance()
     while x<0 or x>=cash:
         print ("INSIRA O VALOR PARA SAQUE:")
         x=float(("(para cancelar a operação, digite 0)\n"))
+    sucessfull(x)
     return cash-x
 def payment ():
+    simulator()
     x=balance()
     print ("INSIRA O VALOR PARA DEPÓSITO:")
     x=float (input ("(para cancelar, digite 0)\n"))
+    sucessfull(x)
     return cash+x
-simulator()
 while op!=0:
+    simulator()
     op=int(input("ESCOLHA UMA OPERAÇÃO:\n1-SALDO\n2-SAQUE\n3-PAGAMENTO\n0-SAIR\n"))
-    if op<0 or op>3
+    if op<0 or op>3:
         print ("insira somente uma das opções de entrada:\n")
-    elif op==1:
+    elif op==1:#saldo
         os.system("cls")
         simulator()
-        e=balance()
-        sucessfull()
-    elif op==2:
-        
+        balance()
+    elif op==2:#saque
+        os.system("cls")
+        cash=withdrawal()
+    elif op==3:#pagamento
+        os.system ("cls")
+        cash=payment()
+    else:
+        break
+print ("fim_alg")
