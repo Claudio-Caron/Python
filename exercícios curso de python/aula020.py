656d9ce18bb4775b39072d8f0b16d240668a8e35
Claudio André Ferreria Caron <140202178+Claudio-Caron@users.noreply.github.com>
2023-09-24 12:00:34 -0400

    Alteração e conserto do código

--- exercícios curso de python/aula020.py
+++ exercícios curso de python/aula020.py
@@ -1,4 +1,5 @@
 import os
+import time
 os.system("cls")
 cash=1500
 op=1
@@ -13,38 +14,50 @@ def sucessfull(y):#mensagem para operação realizada ou não
         print ("\nOPERAÇÃO CANCELADA\n")
     else :
         print ("\nOPERAÇÃO REALIZADA COM SUCESSO!!!")
-    balance()
+
+    print ("aguarde para prosseguir!\n")
+    time.sleep(3)
 def withdrawal ():#saque
     os.system ("cls")
     simulator()
     balance()
+    x=-1
     while x<0 or x>=cash:
         print ("INSIRA O VALOR PARA SAQUE:")
-        x=float(("(para cancelar a operação, digite 0)\n"))
+        x=float(input("(para cancelar a operação, digite 0)\n"))
+        if x<0 or x>=cash:
+            os.system ("cls")
+            print ("operação não pode ser realizada, insira novamente o valor")
     sucessfull(x)
     return cash-x
 def payment ():
     simulator()
-    x=balance()
+    balance()
     print ("INSIRA O VALOR PARA DEPÓSITO:")
-    x=float (input ("(para cancelar, digite 0)\n"))
+    x=float (input("(para cancelar, digite 0)\n"))
     sucessfull(x)
     return cash+x
 while op!=0:
     simulator()
-    op=int(input("ESCOLHA UMA OPERAÇÃO:\n1-SALDO\n2-SAQUE\n3-PAGAMENTO\n0-SAIR\n"))
+    op=int(input("ESCOLHA UMA OPERAÇÃO:\n1-SALDO\n2-SAQUE\n3-DEPÓSITO\n0-SAIR\n"))
+    op=int(op)
     if op<0 or op>3:
         print ("insira somente uma das opções de entrada:\n")
     elif op==1:#saldo
         os.system("cls")
-        simulator()
         balance()
     elif op==2:#saque
         os.system("cls")
         cash=withdrawal()
+        balance()
     elif op==3:#pagamento
         os.system ("cls")
         cash=payment()
+        balance()
     else:
+        os.system ("cls")
+        balance()
+        print ("VOCÊ SAIU DO SISTEMA\n")
         break
+    
 print ("fim_alg")
